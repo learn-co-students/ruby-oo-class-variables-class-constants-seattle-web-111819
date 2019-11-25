1adafc1e055174608cf99035e3a8d386604a9a20
Cole Rau <colevrau@gmail.com>
2019-11-25 10:58:45 -0800

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(@brand)
+      BRANDS << @brand 
+    end 
+    BRANDS 
   end
 
   def cobble
@@ -11,4 +19,15 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(new_brand)
+    @brand = new_brand 
+  end  
 end
+
+
+
+
+
+
+
+
